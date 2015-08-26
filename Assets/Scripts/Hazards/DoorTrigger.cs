262006baa8f0d425394247804668014a6b65b0c6
tobiasazpia <tobiasazpia@hotmail.com>
2015-08-25 17:23:27 -0700

    eugene stuff, and level 2

---
+++ Assets/Scripts/Hazards/DoorTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorTrigger : MonoBehaviour {
+	
+	public Transform door;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player") {
+			door.gameObject.SetActive (true);
+		}
+	}
+
+}
---
+++ Assets/Scripts/Hazards/FireBallHazardGroundMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireBallHazardGroundMovement : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.RotateAround (Vector3.zero, Vector3.up, 20 * Time.deltaTime);
+		//transform.RotateAround (sphereObject.position, Vector3.up, 20 * Time.deltaTime);
+		transform.Rotate(0, 0, 100 * Time.deltaTime);
+	}
+
+
+}
