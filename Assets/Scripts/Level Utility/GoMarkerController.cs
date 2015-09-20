65063f46c1bcb174839c4ced366a6d4b024f243f
Steve <d4rkc1ph3r@gmail.com>
2015-09-20 07:43:39 -0400

    -Added Game Icon and Cursor -Added custom GoMarkers for each themed section

--- Assets/Scripts/Level Utility/GoMarkerController.cs
+++ Assets/Scripts/Level Utility/GoMarkerController.cs
@@ -6,12 +6,41 @@ public class GoMarkerController : MonoBehaviour
 {
     public bool shouldFlash;
     private float displayTime = 2.5f;
+    private Animator anim;
 
 	// Use this for initialization
 	void Start ()
     {
-        shouldFlash = true;   
-  
+        anim = GetComponent<Animator>();
+        //Itachi Marker
+
+        if(Application.loadedLevel == 7 ||
+           Application.loadedLevel == 8 ||
+           Application.loadedLevel == 9)
+        {
+            anim.runtimeAnimatorController = Resources.Load("Animations/GoMarker") as RuntimeAnimatorController;
+        }
+        //Cyborg Marker
+        if (Application.loadedLevel == 10 ||
+           Application.loadedLevel == 11 ||
+           Application.loadedLevel == 12)
+        {
+            anim.runtimeAnimatorController = Resources.Load("Animations/cyborgGoMarker") as RuntimeAnimatorController;
+        }
+        //Sonic Marker
+        if (Application.loadedLevel == 13 ||
+           Application.loadedLevel == 14 ||
+           Application.loadedLevel == 15)
+        {
+            anim.runtimeAnimatorController = Resources.Load("Animations/sonicGoMarker") as RuntimeAnimatorController;
+        }
+        //Mario Marker
+        if(Application.loadedLevel == 16)
+        {
+            //To Be Created
+            return;
+        }
+        shouldFlash = true;
 	}
 	
 	// Update is called once per frame
